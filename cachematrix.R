835928f4a9932dd0c055b500dd13cda03b1de576
cs <christoph.schmidl@gmx.net>
2014-08-24 16:42:29 +0200

    finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
 # makeCacheMatrix creates a list containing a function to
 # 1. set the value of the matrix
 # 2. get the value of the matrix
-# 3. set the value of inverse of the matrix
-# 4. get the value of inverse of the matrix
+# 3. set the value of the matrix inverse
+# 4. get the value of the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
 
   inv <- NULL
